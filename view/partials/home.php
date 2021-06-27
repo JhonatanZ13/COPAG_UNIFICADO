6100879bd2b4d8cfcc54503d0a54359c8364b56a
JhonatanZ13 <zambranojhonatan20@gmail.com>
2021-06-27 09:47:33 -0500

    Fondo animado

--- view/partials/home.php
+++ view/partials/home.php
@@ -1,7 +1,11 @@
 <div class="x_content inicio">
-    <div class="m-auto w-75 ">
-        <div class="text-center contenedor-padre m-auto" id="contenedor-imagen">
-           <img src="images/Bienvenido2.png" alt="" width="500px"> 
+    <div class="m-auto w-75">
+        <div class="col-md-12 container-fluid">
+            <div class="text-center m-auto contenedor-padre" id="contenedor-imagen">
+                <div class="">
+                    <img src="images/Bienvenido2.png" alt="" width="100%" class="img-fluid"> 
+                </div>
+            </div>
         </div>
     </div>
 </div>
